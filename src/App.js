ab283ab6ab784d008359f9766477747e5a555af4
nimapm <nima.gt10@gmail.com>
2022-06-03 17:24:31 +0430

    Change pages link

--- src/App.js
+++ src/App.js
@@ -23,15 +23,14 @@ function App() {
         selectedPage: "",
         intro: "https://nimaprmdi.github.io/npm-screen/",
         learnToUse: "https://nimaprmdi.github.io/npm-screen/",
-        projects: "https://nimaprmdi.github.io/npm-screen/#/projects",
-        portfolio: "https://nimaprmdi.github.io/npm-screen/#/projects",
+        projects: "https://portfoliom.ir/projects/",
+        portfolio: "https://portfoliom.ir/projects/",
         about: "https://nimaprmdi.github.io/npm-screen/#/about",
         blog: "https://portfoliom.ir/blog/",
-        blogSingle: "https://nimaprmdi.github.io/npm-screen/#/blog",
+        blogSingle: "https://portfoliom.ir/blog/",
     });
     const [isAnimationLoaded, setIsAnimationLoaded] = useState(false);
     const [isScreenLoading, setisScreenLoading] = useState(false);
-
     const [isLoading, setIsLoading] = useState(true);
 
     function Loading() {
@@ -45,7 +44,7 @@ function App() {
             <Helmet>
                 <meta charSet="utf-8" />
                 <title>Nima PourMohamadi | Nimapm.com</title>
-                <link rel="canonical" href="https://nimaprmdi.github.io/npm-screen/" />
+                <link rel="canonical" href="https://portfoliom.ir/" />
                 <meta name="description" content="Welcome to Nima PourMohamadi 's Personal Website." />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
